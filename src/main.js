3d5ce759ca3d506bca9ea8f99c9e9863e4f3d5d0
asya <shamalo.n.75@gmail.com>
2025-06-29 19:13:52 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import { getImagesByQuery } from './js/pixabay-api.js';
-import { createGallery, clearGallery, showLoader, hideLoader, showLoadMoreButton,
-  hideLoadMoreButton } from './js/render-functions.js';
+import {
+    createGallery,
+    clearGallery,
+    showLoader,
+    hideLoader,
+    showLoadMoreButton,
+    hideLoadMoreButton } from './js/render-functions.js';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
